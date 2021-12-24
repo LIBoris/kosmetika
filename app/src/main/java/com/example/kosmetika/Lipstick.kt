057fac96d0fe0494c7e0c8b6b49ff53630e31b31
perizat03 <kadyrova_p@auca.kg>
2021-12-25 01:11:41 +0600

    salsa

---
+++ app/src/main/java/com/example/kosmetika/Lipstick.kt
@@ -0,0 +1,11 @@
+package com.example.kosmetika
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Lipstick : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lipstick)
+    }
+}
